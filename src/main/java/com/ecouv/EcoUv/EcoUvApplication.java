c7d38e62d13a7c8e18637b70773db22ec7c2bc7f
Ricardo2099 <rojasricardo104@gmail.com>
2025-10-27 20:30:03 -0600

    inicializando proyecto

---
+++ src/main/java/com/ecouv/EcoUv/EcoUvApplication.java
@@ -0,0 +1,13 @@
+package com.ecouv.EcoUv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoUvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoUvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecouv/EcoUv/EcoUvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecouv.EcoUv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoUvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
